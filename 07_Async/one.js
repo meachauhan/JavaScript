96daf9fd55d2814a99dd1ec7a99ebcb909e28c89
Akash Chauhan <50045501+meachauhan@users.noreply.github.com>
2024-01-22 06:39:40 +0000

    AsyncFunctionnality

---
+++ 07_Async/one.js
@@ -0,0 +1,13 @@
+// JavaScript:
+// 1. Synchronous langauge by default.
+// 2. JS is Singlethreaded langauge by default
+
+// Blocking Code:
+//     1.Block the program flow
+//     2. Read file Sync
+
+// Non Blocking Code:
+//     1. Doesn't Block code exectuion
+//     2. Read file Ansync
+
+
